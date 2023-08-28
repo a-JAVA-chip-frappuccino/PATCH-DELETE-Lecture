997852ac7342c30920b03ea39025f567d2f85b6c
Eleanor Kelman <ekelman3@gmail.com>
2023-08-27 22:13:50 -0400

    prepared lecture

--- src/index.js
+++ src/index.js
@@ -13,16 +13,32 @@ function renderChars(characters) {
         name.textContent = character.first + " " + character.last
         div.appendChild(name)
 
-        // create and render p tag for email
-        const email = document.createElement('p')
-        email.textContent = character.email
-        div.appendChild(email)
-
         // create and render image with alt text
         const img = document.createElement('img')
         img.src = character.image
         img.alt = `picture of ${character.first} ${character.last}`
         div.appendChild(img)
+
+        // create and render likes initialized to zero
+        const likes = document.createElement('div')
+        likes.textContent = character.likes
+        likes.style.color = 'red'
+        likes.addEventListener('click', incrementLikes)
+        div.appendChild(likes)
+
+        function incrementLikes(e) {
+            // perform PATCH request to character's ID
+        }
+
+        // add delete button to character
+        const deleteBtn = document.createElement('button')
+        deleteBtn.textContent = `Delete ${character.first} ${character.last}`
+        deleteBtn.addEventListener('click', deleteChar)
+        div.appendChild(deleteBtn)
+
+        function deleteChar(e) {
+            // perform DELETE request to character's ID
+        }
 
     })
 }
